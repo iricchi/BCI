181ca4c6f66a855ebb5201bee87620dfabfd1dbb
Flavio Martinelli <flavio.martinelli.95@gmail.com>
2018-06-04 15:45:51 +0200

    deleted all the shit
    ciaone

--- Elisabetta files/pwelch_for_each_channel.m
+++
@@ -1,9 +0,0 @@
-function [pow, freq]=pwelch_for_each_channel(num_channel,epoched_data,f,fs)
-    pow=0;
-    for i=1:size(epoched_data.data,3)
-        [a,b]=pwelch(squeeze(epoched_data.data(:,num_channel,i)),0.5*fs,0.5*0.5*fs,f,fs);
-        pow=pow+a;
-    end
-    pow=pow/i;
-    freq=b;
-end
